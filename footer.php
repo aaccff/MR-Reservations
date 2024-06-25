b727ffdbea63cd97774d844b106a84092d11f200
aaccff <72335700+aaccff@users.noreply.github.com>
2024-06-25 22:57:39 +0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer>
+        <p>&copy; 2024 Maldives Resorts</p>
+    </footer>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/slider.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/fetchResorts.js"></script>
+</body>
+</html>
