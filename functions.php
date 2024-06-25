9273fd340db42d6950c54e9b29232f6ad1f2f216
aaccff <72335700+aaccff@users.noreply.github.com>
2024-06-26 02:04:33 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -57,3 +57,11 @@ function mr_reservations_widgets_init() {
     ) );
 }
 add_action( 'widgets_init', 'mr_reservations_widgets_init' );
+
+function mr_reservations_enqueue_styles() {
+    wp_enqueue_style( 'mr-reservations-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'mr-reservations-responsive', get_template_directory_uri() . '/css/responsive.css' );
+    wp_enqueue_style( 'mr-reservations-styles', get_template_directory_uri() . '/css/styles.css' );
+}
+add_action( 'wp_enqueue_scripts', 'mr_reservations_enqueue_styles' );
+
