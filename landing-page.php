b8e978c2f47d4d5190ff0c68f7844b5671fffaaa
aaccff <72335700+aaccff@users.noreply.github.com>
2024-06-25 22:58:48 +0500

    Create landing-page.php

---
+++ landing-page.php
@@ -0,0 +1,11 @@
+<?php
+/* Template Name: Landing Page */
+get_header(); ?>
+
+<main>
+    <section id="resort-slider">
+        <!-- Resorts will be loaded here -->
+    </section>
+</main>
+
+<?php get_footer(); ?>
