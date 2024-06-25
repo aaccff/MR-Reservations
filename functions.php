4f201a83d66e5bd271cc7c4a3ba3933f1c14fbd2
aaccff <72335700+aaccff@users.noreply.github.com>
2024-06-26 01:56:10 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,3 +14,46 @@ function mr_reservations_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'mr_reservations_enqueue_scripts');
 ?>
+
+function mr_reservations_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'Sidebar', 'mr_reservations' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Add widgets here to appear in your sidebar.', 'mr_reservations' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Footer 1', 'mr_reservations' ),
+        'id'            => 'footer-1',
+        'description'   => __( 'Add widgets here to appear in your footer.', 'mr_reservations' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Footer 2', 'mr_reservations' ),
+        'id'            => 'footer-2',
+        'description'   => __( 'Add widgets here to appear in your footer.', 'mr_reservations' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Footer 3', 'mr_reservations' ),
+        'id'            => 'footer-3',
+        'description'   => __( 'Add widgets here to appear in your footer.', 'mr_reservations' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'mr_reservations_widgets_init' );
