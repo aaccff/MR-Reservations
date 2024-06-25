5a0889c20752f1371094e32a42c1bd589d071f33
aaccff <72335700+aaccff@users.noreply.github.com>
2024-06-25 23:04:18 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+    <h1>Welcome to MR Reservations</h1>
+    <p>This is the default template. Please navigate to the specific pages for more functionality.</p>
+</main>
+
+<?php get_footer(); ?>
