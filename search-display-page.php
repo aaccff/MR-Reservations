65a15f01caf9a145546607d9c59083c142b11c0b
aaccff <72335700+aaccff@users.noreply.github.com>
2024-06-25 23:00:03 +0500

    Create search-display-page.php

---
+++ search-display-page.php
@@ -0,0 +1,16 @@
+<?php
+/* Template Name: Search Display Page */
+get_header(); ?>
+
+<main>
+    <section id="search-filters">
+        <!-- Search filters form -->
+    </section>
+    <section id="resort-listing">
+        <!-- Resort listings will be loaded here -->
+    </section>
+</main>
+
+<script src="<?php echo get_template_directory_uri(); ?>/js/filters.js"></script>
+
+<?php get_footer(); ?>
