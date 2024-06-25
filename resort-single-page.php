549097d721e4a35a54ff15f3756ce07f99a85fc6
aaccff <72335700+aaccff@users.noreply.github.com>
2024-06-25 22:59:27 +0500

    Create resort-single-page.php

---
+++ resort-single-page.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Resort Single Page */
+get_header(); ?>
+
+<main>
+    <section id="resort-details">
+        <!-- Resort details will be loaded here -->
+    </section>
+</main>
+
+<script src="<?php echo get_template_directory_uri(); ?>/js/resortDetails.js"></script>
+
+<?php get_footer(); ?>
