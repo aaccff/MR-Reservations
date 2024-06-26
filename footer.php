ba7d9092e5bddf6b85aa575074ab83a0c90a97e5
aaccff <72335700+aaccff@users.noreply.github.com>
2024-06-26 17:29:28 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,23 +1,23 @@
-    <footer>
-        <div class="footer-widgets">
-            <?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-                <div class="footer-widget">
-                    <?php dynamic_sidebar( 'footer-1' ); ?>
-                </div>
-            <?php endif; ?>
-            <?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
-                <div class="footer-widget">
-                    <?php dynamic_sidebar( 'footer-2' ); ?>
-                </div>
-            <?php endif; ?>
-            <?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
-                <div class="footer-widget">
-                    <?php dynamic_sidebar( 'footer-3' ); ?>
-                </div>
-            <?php endif; ?>
-        </div>
-        <p>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
-    </footer>
-    <?php wp_footer(); ?>
+<footer>
+    <div class="footer-widgets">
+        <?php if (is_active_sidebar('footer-1')) : ?>
+            <div class="footer-widget">
+                <?php dynamic_sidebar('footer-1'); ?>
+            </div>
+        <?php endif; ?>
+        <?php if (is_active_sidebar('footer-2')) : ?>
+            <div class="footer-widget">
+                <?php dynamic_sidebar('footer-2'); ?>
+            </div>
+        <?php endif; ?>
+        <?php if (is_active_sidebar('footer-3')) : ?>
+            <div class="footer-widget">
+                <?php dynamic_sidebar('footer-3'); ?>
+            </div>
+        <?php endif; ?>
+    </div>
+    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+</footer>
+<?php wp_footer(); ?>
 </body>
 </html>
